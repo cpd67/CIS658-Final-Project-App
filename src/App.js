5e124b9a026edd877d1ebf3db35189655a2a848e
cpd67 <codemonkey941@gmail.com>
2020-04-14 20:09:42 -0400

    Combine Navbar & start linking pages.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import * as React from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './App.css';
+import { Navbar } from "./components/Navbar";
+import { Categories } from './components/Categories';
+import { Expenses } from './components/Expenses';
 
 function App() {
+  const routes = [
+    {
+      label: "Money Trail",
+      path: "/",
+      exact: true,
+      content: () => <Expenses userId={1} />
+    },
+    {
+      label: "Expenses",
+      path: "/expenses",
+      exact: true,
+      content: () => <Expenses userId={1} />
+    },
+    {
+      label: "Categories",
+      path: "/categories",
+      exact: true,
+      content: () => <Categories />
+    }
+  ]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+        <Navbar routes={routes} />
+        <Switch>
+          {routes.map((route, index) =>
+            <Route
+              key={index}
+              path={route.path}
+              exact={route.exact}
+              children={route.content}
+            />
+          )}
+        </Switch>
+    </Router>
+  )
 }
 
 export default App;
